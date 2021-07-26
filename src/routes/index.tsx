4a80255a657e77455601582b3da9434ea6741682
Yago Juan <yago.juan@teclat.com.br>
2021-07-26 02:17:20 -0300

    Adiciona a funcionalidade de logar com o Discord

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+
 import AuthRoutes from "./auth.routes";
 
+import SignIn from "../screens/SignIn";
+
+import { useAuth } from "../hooks/auth";
+
 export default function Routes() {
+  const { user } = useAuth();
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      {user.token ? <AuthRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 }
